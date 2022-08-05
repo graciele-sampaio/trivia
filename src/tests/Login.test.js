8ead129717e60ec7db40984a5057f06975c21597
Emily Menezes <estudosmenezes@gmail.com>
2022-08-05 18:25:47 -0300

    Requisito 4

---
+++ src/tests/Login.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+
+describe('', () => {
+    it('', () => {
+      renderWithRouterAndRedux(<App />);
+      const fieldName = screen.getByTestId('input-player-name');
+      const fieldEmail = screen.getByTestId('input-gravatar-email');
+
+      expect(fieldName).toBeInTheDocument();
+      expect(fieldEmail).toBeInTheDocument();
+    })
+
+    it('', () => {
+    renderWithRouterAndRedux(<App />);
+    const fieldName = screen.getByTestId('input-player-name');
+    const fieldEmail = screen.getByTestId('input-gravatar-email');
+    const buttonPlay = screen.getByTestId('btn-play');
+  
+    expect(buttonPlay).toBeDisabled();
+
+    userEvent.type(fieldName, 'Nome');
+    expect(buttonPlay).toBeDisabled();
+
+    userEvent.type(fieldEmail, 'alguem@email.com');
+    expect(buttonPlay).not.toBeDisabled();
+  });
+})
