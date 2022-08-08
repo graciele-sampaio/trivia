2314c0800b38f846689b6b02747081e07510f2c9
Emily Menezes <estudosmenezes@gmail.com>
2022-08-08 19:17:48 -0300

    Corrigindo lint

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -15,92 +15,96 @@ class Game extends Component {
   }
 
   requestQuests = async () => {
-    const { getToken } = this.props;    
+    const { getToken } = this.props;
     const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${getToken}`);
     const json = await response.json();
-    if(json.response_code === 0) this.sucessRequest(json.results);
-    if(json.response_code !== 0) this.failedRequest();
+    if (json.response_code === 0) this.sucessRequest(json.results);
+    if (json.response_code !== 0) this.failedRequest();
   }
 
   sucessRequest = (results) => {
-    this.setState({questions:results, load: true});
+    this.setState({ questions: results, load: true });
   }
 
   failedRequest = () => {
     const { history } = this.props;
     localStorage.removeItem('token');
-    history.push('/');    
+    history.push('/');
   }
 
   handleClick = () => {
     this.setState(
       (previousState) => ({
         order: previousState.order + 1,
-      })
+      }),
     );
   }
 
   handleDataTestId = (index, correctAnswer, currentAnswer) => {
-    if(currentAnswer===correctAnswer) {return 'correct-answer'}
-    else {return `wrong-answer-${index}`}
+    if (currentAnswer === correctAnswer) { return 'correct-answer'; }
+    return `wrong-answer-${index}`;
   };
 
   shuffleAnswers = (array) => {
-    const arrayOrdered = array.sort(() => (Math.round(Math.random())-0.5));
+    const number = 0.5;
+    const arrayOrdered = array.sort(() => (Math.round(Math.random()) - number));
     return arrayOrdered;
   }
 
-  booleanQuestion = (correctAnswer) => {
-    return this.shuffleAnswers([
-      (<button
+  booleanQuestion = (correctAnswer) => this.shuffleAnswers([
+    (
+      <button
+        type="button"
         data-testid={ this.handleDataTestId(0, correctAnswer, 'True') }
         key={ this.handleDataTestId(0, correctAnswer, 'True') }
       >
         True
-      </button>),
-      (<button
+      </button>
+    ),
+    (
+      <button
+        type="button"
         data-testid={ this.handleDataTestId(1, correctAnswer, 'False') }
         key={ this.handleDataTestId(0, correctAnswer, 'False') }
       >
         False
-      </button>)
-    ]);
-  }
+      </button>
+    ),
+  ])
 
-  multipleQuestion = (question, correctAnswer) => {
-    return this.shuffleAnswers([
-        ...question.incorrect_answers,
-        correctAnswer,
-      ]).map((item, index) => (
-        <button
-          key={ this.handleDataTestId(index, correctAnswer, item) }
-          data-testid={ this.handleDataTestId(index, correctAnswer, item) }
-        >
-          {item}
-        </button>
-      ));
-  }
+  multipleQuestion = (question, correctAnswer) => this.shuffleAnswers([
+    ...question.incorrect_answers,
+    correctAnswer,
+  ]).map((item, index) => (
+    <button
+      type="button"
+      key={ this.handleDataTestId(index, correctAnswer, item) }
+      data-testid={ this.handleDataTestId(index, correctAnswer, item) }
+    >
+      {item}
+    </button>
+  ))
 
   renderQuestion = (questions, order) => {
     const question = questions[order];
     const correctAnswer = question.correct_answer;
     return (
       <div>
-        <p data-testid='question-category'>{question.category}</p>
-        <p data-testid='question-text'>{`Pergunta: ${question.question}`}</p>
-        <span data-testid='answer-options'>
+        <p data-testid="question-category">{question.category}</p>
+        <p data-testid="question-text">{`Pergunta: ${question.question}`}</p>
+        <span data-testid="answer-options">
           {
             (question.type === 'multiple')
-            ?  this.multipleQuestion(question, correctAnswer)
-            :  this.booleanQuestion(correctAnswer)
+              ? this.multipleQuestion(question, correctAnswer)
+              : this.booleanQuestion(correctAnswer)
           }
-        </span>     
+        </span>
       </div>
-    )
+    );
   }
 
   render() {
-    const {load, questions, order} = this.state;
+    const { load, questions, order } = this.state;
 
     return (
       <div data-testid="settings-title">
@@ -108,9 +112,9 @@ class Game extends Component {
         <Header />
         {(load && questions.length > 0)
           ? this.renderQuestion(questions, order)
-          : <h1>Carregando...</h1>
-        }
+          : <h1>Carregando...</h1>}
         <button
+          type="button"
           onClick={ this.handleClick }
         >
           Próxima Questão
